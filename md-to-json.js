5ae555e44ffd5d0f6377af144b1c372483e112f2
olagato <olagato@gmail.com>
2023-08-04 12:31:51 +0200

    chore: commit save point

--- md-to-json.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var MarkdownIt = require('markdown-it');
-var md = new MarkdownIt();
-
-var toc = fs.readFileSync('./toc.md', 'utf8');
-var result = md.parse(toc);
-
-fs.writeFileSync( "./toc.json", JSON.stringify(result) );
